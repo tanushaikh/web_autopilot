b4f67a21c5212e02d99534629c25beecbcf573bd
Kiran jadhav <kiranjadhav9561@gmail.com>
2023-08-25 22:04:29 +0530

    added file for anuj

---
+++ src/components/auth/login/LoginA.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ProductImage from "../register/ProductImage"
+const LoginA = () => {
+    return (
+        <div className='row'>
+            <div className="col-lg-12">
+                <ProductImage />
+            </div>
+            <div className="col-lg-12">
+                anuj work here
+            </div>
+        </div>
+    )
+}
+
+export default LoginA
