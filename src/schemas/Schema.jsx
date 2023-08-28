4429044cdfb49515a4545d77996651cef78b429e
Kiran jadhav <kiranjadhav9561@gmail.com>
2023-08-28 21:58:12 +0530

    fixed register on submit

--- src/schemas/Schema.jsx
+++ src/schemas/Schema.jsx
@@ -14,10 +14,10 @@ export const signUpSchema = Yup.object({
     username: Yup.string().min(2).max(25).required("Please enter your name"),
     email: Yup.string().email().required("Please enter your email"),
     password: Yup.string().min(6).max(14).required("Please enter your password"),
-    confirm_password: Yup.string()
-        .required()
-        .oneOf([Yup.ref("password"), null], "Password must match"),
-    category: Yup.string().required("Please enter your category"),
+    // confirm_password: Yup.string()
+    //     .required()
+    //     .oneOf([Yup.ref("password"), null], "Password must match"),
+    // category: Yup.string().required("Please enter your category"),
     TermsAndConditions: Yup.boolean()
         .oneOf([true], 'You must accept the terms and conditions')
         .required('You must accept the terms and conditions'),
@@ -38,5 +38,5 @@ export const SetNewPasswordSchema = Yup.object({
 
 // Set Your New Password
 export const verifyOtpSchema = Yup.object({
-    otp: Yup.number().min(6).max(6).required("Please enter your 6 digit otp"),
+    otp: Yup.number().required("Please enter your 6 digit otp"),
 });
