e9d9dd91872299fe14d6b6b73609220cefb2167b
Kiran jadhav <kiranjadhav9561@gmail.com>
2023-09-06 16:12:14 +0530

    added search box in navbar

--- src/pages/shop/componts/reusable/navbar/NavBar.jsx
+++ src/pages/shop/componts/reusable/navbar/NavBar.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import BellIcon from "../../../../../icons/BellIcon";
+import SearchIcon from "../../../../../icons/SearchIcon";
 
 const NavBar = () => {
   return (
     <div className="p-2 container-navbar-main">
       <div className="d-flex justify-content-between align-items-center">
-        <div>Search</div>
+        <div>
+          <div className="navbar-search-input-container">
+            <span>
+              <SearchIcon />
+            </span>
+            <input className="navbar-search-input-box" type="text" />
+          </div>
+        </div>
         <div className="d-flex align-items-center">
           <div>
             <BellIcon />
